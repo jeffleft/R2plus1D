54242cb655baefed741673ec2032788a0c27ffb8
jewang <jewang@jewang-3149.pods.jewang.svc.cluster.local>
2018-06-10 11:47:08 -0700

    modified script

--- scripts/my_train_hmdb51.sh
+++ scripts/my_train_hmdb51.sh
@@ -2,7 +2,7 @@ python tools/train_net.py \
 --train_data=../datasets/hmdb51_train01 \
 --test_data=../datasets/hmdb51_test01 \
 --model_name=r2plus1d --model_depth=18 \
---clip_length_rgb=32 --batch_size=8 \
+--clip_length_rgb=32 --batch_size=4 \
 --gpus=0,1 --base_learning_rate=0.002 \
 --epoch_size=20000 --num_epochs=14 --step_epoch=2 \
 --weight_decay=0.0005 --num_labels=51
