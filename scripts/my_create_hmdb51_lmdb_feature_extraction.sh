bceb4d200ea0894d35d488372c2f10230310a9e7
jewang <jewang@jewang-7465.pods.jewang.svc.cluster.local>
2018-06-11 14:25:41 -0700

    testing feature extraction

---
+++ scripts/my_create_hmdb51_lmdb_feature_extraction.sh
@@ -0,0 +1,4 @@
+python data/create_video_db.py \
+--list_file=data/list/hmdb51/hmdb51_test_01_video_id_dense_l32_2.csv \
+--output_file=../datasets/hmdb51_feature_extraction/hmdb51_test_01_video_id_dense_l32_2 \
+--use_list=1 --use_video_id=1 --use_start_frame=1
--- scripts/my_finetune_hmdb51.sh
+++ scripts/my_finetune_hmdb51.sh
@@ -5,6 +5,6 @@ python tools/train_net.py \
 --clip_length_rgb=32 --batch_size=4 \
 --pretrained_model=../r2.5d_d34_l32_ft_sports1m.pkl \
 --db_type='pickle' --is_checkpoint=0 \
---gpus=0,1 --base_learning_rate=0.0002 \
---epoch_size=40000 --num_epochs=7 --step_epoch=2 \
+--gpus=0,1 --base_learning_rate=0.0008 \
+--epoch_size=20000 --num_epochs=14 --step_epoch=5 \
 --weight_decay=0.005 --num_labels=51
